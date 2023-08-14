d516a9570b05b859e9467c0eec24065309ce24e1
sandaruanjana <sandaruanjana@outlook.com>
2023-08-14 20:44:11 +0530

    initial commit

---
+++ src/main/java/ac/uk/bolton/onlinerecipesharingplatform/OnlineRecipeSharingPlatformApplication.java
@@ -0,0 +1,13 @@
+package ac.uk.bolton.onlinerecipesharingplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineRecipeSharingPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineRecipeSharingPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ac/uk/bolton/onlinerecipesharingplatform/OnlineRecipeSharingPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.uk.bolton.onlinerecipesharingplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineRecipeSharingPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
